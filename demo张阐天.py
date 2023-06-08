dce7ff669c4049d3d4334a282388d75b5b872783
zzzcccttt <135958681+zzzcccttt@users.noreply.github.com>
2023-06-08 17:47:38 +0800

    Update demo张阐天.py

--- demo张阐天.py
+++ demo张阐天.py
@@ -20,7 +20,7 @@ def load_data():
     data_df = data_df[data_df['rating'] != 99.0]
 
     # Load the jokes
-     jokes_df = pd.read_excel('Dataset4JokeSet.xlsx', header=None)
+    jokes_df = pd.read_excel('Dataset4JokeSet.xlsx', header=None)
     jokes_df.columns = ['joke']
     jokes_df.index.name = 'joke_id'
     return data_df, jokes_df
